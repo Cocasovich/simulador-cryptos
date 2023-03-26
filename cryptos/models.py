12103d5f954e4ab8f480da9a86ae2da5b6b403d7
Cocasovich <claudia.docu.perezdom@gmail.com>
2023-03-26 19:38:45 +0200

    feat: primera maqueta finalizada página de movs

--- cryptos/models.py
+++ cryptos/models.py
@@ -1,10 +1,20 @@
 import csv
 
+from datetime import date
+
 from . import RUTA_FICHERO
 
 class Movimiento:
     def __init__(self, fecha, hora, divisa_1 , cantidad_1, divisa_2, cantidad_2, P_U ):
-        self.fecha = fecha
+        
+        self.errores = []
+
+        try:
+            self.fecha = date.fromisoformat(fecha)
+        except ValueError:
+            self.fecha = None
+            self.errores.append("El formato de la fecha no es válido")
+
         self.hora = hora
         self.divisa_1 = divisa_1
         self.cantidad_1 = cantidad_1
@@ -12,7 +22,10 @@ class Movimiento:
         self.cantidad_2 = cantidad_2
         self.P_U = P_U
 
-
+    @property
+    def has_errors(self):
+        return len(self.errores) > 0
+    
 class ListaMovimientos:
     """
     Almacenar ls lista con todos los movimientos
@@ -25,4 +38,12 @@ class ListaMovimientos:
         with open(RUTA_FICHERO, 'r') as fichero:
             reader = csv.DictReader(fichero)
             for fila in reader:
-                self.lista_movimientos.append(fila)
+                mov = Movimiento(
+                    fila["fecha"], 
+                    fila["hora"], 
+                    fila["divisa_1"], 
+                    fila["cantidad_1"],
+                    fila["divisa_2"],
+                    fila["cantidad_2"],
+                    fila["P_U"])
+                self.lista_movimientos.append(mov)
