2353e5315da4201c5ac06a3c2972610eda11e7cd
Claudia Perez <claudia.docu.perezdom@gmail.com>
2023-04-09 20:35:04 +0200

    feat: creamos estado

--- cryptos/tools.py
+++ cryptos/tools.py
@@ -7,3 +7,15 @@ def cantidad_disponible(moneda, movimientos):
         if fila["moneda_from"] == moneda:
             total = total - fila["cantidad_from"]
     return total
+
+
+def saldo_euros_invertidos(movimientos):
+    total_to_euros = 0
+    total_from_euros = 0
+    for fila in movimientos:
+        if fila["moneda_to"] == "EUR":
+            total_to_euros += fila["cantidad_to"]
+        if fila["moneda_from"] == "EUR":
+            total_from_euros += fila["cantidad_from"]
+    saldo = total_to_euros - total_from_euros
+    return total_from_euros, saldo
--- cryptos/views.py
+++ cryptos/views.py
@@ -6,11 +6,13 @@ from flask import render_template, redirect, request, url_for, flash
 from . import app
 from .forms import MovimientoForm
 from .models import DBManager, APIManager, APIException
-from .tools import cantidad_disponible
+from .tools import cantidad_disponible, saldo_euros_invertidos
 
-RUTA =  os.path.join('cryptos/data','simulador-cryptos.db')
+RUTA = os.path.join('cryptos/data', 'simulador-cryptos.db')
 db = DBManager(RUTA)
-api_manager = APIManager(app.config.get("COIN_API_URL"), app.config.get("API_KEY"))
+api_manager = APIManager(app.config.get(
+    "COIN_API_URL"), app.config.get("API_KEY"))
+
 
 @app.route('/')
 def home():
@@ -18,43 +20,47 @@ def home():
     movimientos = db.consultaSQL(consulta)
     return render_template("inicio.html", movs=movimientos)
 
+
 @app.route('/nuevo', methods=['GET', 'POST'])
 def new_movement():
 
     if request.method == 'GET':
         formulario = MovimientoForm()
         return render_template('form_movimiento.html', form=formulario)
-    
+
     else:
         formulario = MovimientoForm(data=request.form)
         if formulario.validate_on_submit():
             if formulario.data["calcular"]:
-                
+
                 if formulario.data.get("moneda_from") != 'EUR':
                     consulta = 'SELECT moneda_from, cantidad_from, moneda_to, cantidad_to FROM movimientos'
                     movimientos = db.consultaSQL(consulta)
-                    
-                    total_disponible = cantidad_disponible(formulario.data.get("moneda_from"), movimientos)
+
+                    total_disponible = cantidad_disponible(
+                        formulario.data.get("moneda_from"), movimientos)
                     if total_disponible < formulario.data.get("cantidad_from"):
-    
-                        flash(f"La cantidad de {formulario.data.get('moneda_from')} no es suficiente")
+
+                        flash(
+                            f"La cantidad de {formulario.data.get('moneda_from')} no es suficiente")
                         return render_template("form_movimiento.html", form=formulario)
-                
-                
-                try: 
+
+                try:
                     cantidad_to = api_manager.calcular_tasa(formulario.data.get("moneda_from"),
-                                                            formulario.data.get("cantidad_from"),
+                                                            formulario.data.get(
+                                                                "cantidad_from"),
                                                             formulario.data.get("moneda_to"))
-            
+
                 except APIException as error:
                     print("Se ha producido un error al consultar la tasa:", error)
-                    flash(f"Se ha producido un error al consultar la tasa: {error}")
+                    flash(
+                        f"Se ha producido un error al consultar la tasa: {error}")
                     return render_template("form_movimiento.html", form=formulario)
                 except requests.RequestException as error:
                     print("Se ha producido un error al consultar la tasa:", error)
                     flash(f"Se ha producido un error al consultar la tasa")
                     return render_template("form_movimiento.html", form=formulario)
-                    
+
                 formulario.cantidad_to.data = cantidad_to
                 formulario.cantidad_toH.data = cantidad_to
 
@@ -66,22 +72,48 @@ def new_movement():
                 hora = now.strftime("%H:%M:%S")
                 cantidad_from = float(formulario.cantidad_from.data)
                 cantidad_to = float(formulario.cantidad_toH.data)
-                precio_unitario = cantidad_from/cantidad_to                
+                precio_unitario = cantidad_from/cantidad_to
                 db.consultaConParametros("""INSERT INTO movimientos (fecha, hora, moneda_from,cantidad_from,moneda_to,cantidad_to,precio_unitario) VALUES (?,?,?,?,?,?,?)""",
-                                         [fecha, hora,formulario.moneda_from.data,
-                                         formulario.cantidad_from.data,
-                                         formulario.moneda_to.data,
-                                         float(formulario.cantidad_toH.data),
-                                         precio_unitario])
-                
+                                         [fecha, hora, formulario.moneda_from.data,
+                                          formulario.cantidad_from.data,
+                                          formulario.moneda_to.data,
+                                          float(formulario.cantidad_toH.data),
+                                          precio_unitario])
+
                 return redirect(url_for("home"))
 
         else:
             return render_template("form_movimiento.html", form=formulario)
 
 
-
 @app.route('/estado')
 def state():
-    return "Estado de la inversion"
+    consulta = 'SELECT moneda_from, cantidad_from, moneda_to, cantidad_to FROM movimientos'
+    movimientos = db.consultaSQL(consulta)
 
+    cryptos = []
+    for fila in movimientos:
+        if fila["moneda_from"] != "EUR" and fila["moneda_from"] not in cryptos:
+            cryptos.append(fila["moneda_from"])
+        if fila["moneda_to"] != "EUR" and fila["moneda_to"] not in cryptos:
+            cryptos.append(fila["moneda_to"])
+
+    wallet = {}
+    for crypto in cryptos:
+        wallet[crypto] = cantidad_disponible(crypto, movimientos)
+
+    total_cypto_a_euro = 0
+    for clave, valor in wallet.items():
+
+        valor_wallet_euros = api_manager.calcular_tasa(clave, valor, 'EUR')
+        total_cypto_a_euro = valor_wallet_euros + total_cypto_a_euro
+
+    total_from_euros, saldo = saldo_euros_invertidos(movimientos)
+
+    print(total_from_euros)
+    print(saldo)
+    print(total_cypto_a_euro)
+
+    total_inversion = total_from_euros + saldo + total_cypto_a_euro
+
+    return str(total_inversion)
